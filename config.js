77c4a473d56fdc7f64a601f159701ac960552265
iv1234 <39189912+iv1234@users.noreply.github.com>
2018-05-11 21:20:25 +0000

    test7

--- config.js
+++ config.js
@@ -1,21 +1,12 @@
 var Config = {
-  // Contents of this file will be send to the client
-  "domain":     process.env.OPENSHIFT_APP_DNS || 'test3-hicc.a3c1.starter-us-west-1.openshiftapps.com',
-
-  "serverip":   process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
-  
-  "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8080':'8080',
-  "protocol":   'ws://',
-
-  "heartbeattmo": 30000, // milliseconds 
-  
-  "wsclientopts": { reconnection: true, 
-                    reconnectionDelay: 2000,
-                    reconnectionAttempts: 100,
-                    secure: false
-                  }
+    "serverip": process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
+    "heartbeattmo": 500000, // milliseconds 
+    "wsclientopts": {
+        reconnection: true,
+        reconnectionDelay: 2000,
+        reconnectionAttempts: 100,
+        secure: false
+    }
 };
-
 module.exports = Config;
-
