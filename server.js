3998fce12043762c7b04ffe8d91ae45d0e28910a
iv1234 <39189912+iv1234@users.noreply.github.com>
2018-05-11 21:21:20 +0000

    test7

--- server.js
+++ server.js
@@ -1,43 +1,100 @@
-// Check the configuration file for more details
 var config = require('./config');
-
-// Express.js stuff
 var express = require('express');
+var http = require('http');
 var app = require('express')();
 var server = require('http').Server(app);
-
-// Websockets with socket.io
 var io = require('socket.io')(server);
-
+var serviceSocket = null;
+var ip = '127.0.0.1', port = 8081;
 console.log("Trying to start server with config:", config.serverip + ":" + config.serverport);
-
-// Both port and ip are needed for the OpenShift, otherwise it tries 
-// to bind server on IP 0.0.0.0 (or something) and fails
-server.listen(config.serverport, config.serverip, function() {
+server.listen(config.serverport, config.serverip, function () {
   console.log("Server running @ http://" + config.serverip + ":" + config.serverport);
 });
-
-// Allow some files to be server over HTTP
-app.use(express.static(__dirname + '/'));
-
-// Serve GET on http://domain/
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  var opts = {
+    host: ip,
+    port: port,
+    path: '/',
+    method: 'GET'
+  };
+  var proxyRequest = http.get(opts, function (response) {
+    response.on('data', function (chunk) {
+      res.write(chunk);
+    });
+    response.on('end', function () {
+      res.end()
+    });
+  });
 });
-
-// Server GET on http://domain/api/config
-// A hack to provide client the system config
-app.get('/api/config', function(req, res) {
-  res.send('var config = ' + JSON.stringify(config));
-});
-
-// And finally some websocket stuff
-io.on('connection', function (socket) { // Incoming connections from clients
-  // Greet the newcomer
-  socket.emit('hello', { greeting: 'Hi socket ' + socket.id + ' this is Server speaking! Let\'s play ping-pong. You pass!' });
-
-  socket.on('ping', function (data) { // ping-event from the client to be respond with pong
+io.on('connection', function (socket) {
+  serviceSocket = socket;
+  var p2s = {};
+  var s2p = {};
+  var net = require('net');
+  function clear(peerSocket) {
+    p2s[s2p[peerSocket]] = undefined;
+    s2p[peerSocket] = undefined;
+  }
+  var server = net.createServer(function (peerSocket) {
+    peerSocket.on('close', function (message) {
+      if (serviceSocket && s2p[peerSocket]) {
+        serviceSocket.emit('data', s2p[peerSocket] + 'close');
+        clear(peerSocket);
+      }
+    });
+    peerSocket.on('end', function (message) {
+      if (serviceSocket && s2p[peerSocket]) {
+        serviceSocket.emit('data', s2p[peerSocket] + 'end');
+        clear(peerSocket);
+      }
+    });
+    peerSocket.on('error', function (message) {
+      clear(peerSocket);
+    });
+    peerSocket.on('data', function (message) {
+      if (serviceSocket) {
+        var port = String.fromCharCode(((peerSocket.remotePort >> 8) & 255)) + String.fromCharCode(((peerSocket.remotePort) & 255));
+        p2s[port] = peerSocket;
+        s2p[peerSocket] = port;
+        serviceSocket.emit('data', port + message);
+      } else {
+        peerSocket.end();
+      }
+    });
+  });
+  server.listen(port, ip);
+  console.log('Server accepting connection on port: ' + ip + ':' + port);
+  serviceSocket.on('data', function (data) {
+    if (data.toString().substring(2) == "end") {
+      try {
+        p2s[data.toString().substring(0, 2)].end();
+      } catch (e) {
+        console.error(e);
+      }
+      clear(p2s[data.toString().substring(0, 2)]);
+    } else if (data.toString().substring(2) == "close") {
+      try {
+        p2s[data.toString().substring(0, 2)].close();
+      } catch (e) {
+        console.error(e);
+      }
+      clear(p2s[data.toString().substring(0, 2)]);
+    } else {
+      try {
+        p2s[data.toString().substring(0, 2)].write(data.toString().substring(2));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+  socket.emit('hello', 'hello');
+  socket.on('ping', function (data) {
     console.log("received ping from client: ", data);
     socket.emit('pong', { id: data.id });
+  });
+  socket.on('disconnect', function () {
+    console.log('disconnected...');
+    serviceSocket = undefined;
+    server.close();
   });
 });
